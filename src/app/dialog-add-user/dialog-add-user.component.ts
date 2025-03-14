f728c5da7daa29f81bbc3db2be20c441d30eb765
Martin Bock <martinbock@arcor.de>
2025-03-14 12:35:14 +0100

    "dialog.component created, inputfields created & styled"

---
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogContent } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCalendar, MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
+
+@Component({
+  selector: 'app-dialog-add-user',
+  standalone: true,
+  providers: [provideNativeDateAdapter()],
+  imports: [
+    MatButtonModule,
+    MatDialogContent,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
+  templateUrl: './dialog-add-user.component.html',
+  styleUrl: './dialog-add-user.component.scss',
+})
+export class DialogAddUserComponent {}
