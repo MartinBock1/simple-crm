1b314761909fa66c4b98b23f077db464871b02d7
Martin Bock <martinbock@arcor.de>
2025-03-14 17:30:43 +0100

    "firebase implemented, value of inputfields can be stored in database, progressbar inplemented"

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,28 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideAnimationsAsync(),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'simple-crm-aae5e',
+          appId: '1:471885741953:web:c2fe6c7b7cd2484ae37280',
+          storageBucket: 'simple-crm-aae5e.firebasestorage.app',
+          apiKey: 'AIzaSyBK2zvLDWDivgpiWiuziZUswjx59gEjA6U',
+          authDomain: 'simple-crm-aae5e.firebaseapp.com',
+          messagingSenderId: '471885741953',
+        })
+      )
+    ),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+  ],
 };
--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogContent } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
@@ -6,20 +6,59 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCalendar, MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { User } from '../../models/user.class';
+import { FormsModule } from '@angular/forms';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
   providers: [provideNativeDateAdapter()],
   imports: [
+    FormsModule,
     MatButtonModule,
     MatDialogContent,
     MatInputModule,
     MatFormFieldModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    MatProgressBarModule,
   ],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss',
 })
-export class DialogAddUserComponent {}
+export class DialogAddUserComponent {
+  user = new User();
+  birthDate!: Date;
+  firestore: Firestore = inject(Firestore);
+  isLoading = false;
+
+  // loading() {
+  //   this.isLoading = !this.isLoading;
+  // }
+
+  saveUser() {
+    this.user.birthDate = this.birthDate.getTime();
+    console.log('Current user is: ', this.user);
+
+    this.isLoading = true;
+
+    // this.firestore
+    //   .collection('users')
+    //   .add(this.user.toJSON())
+    //   .then((result: any) => {
+    //     console.log('adding user finished ', result);
+    //   });
+    // Get the Firestore instance correctly
+    const firestoreInstance = this.firestore;
+
+    // Use the collection method and addDoc to add the user
+    addDoc(collection(this.firestore, 'users'), this.user.toJSON()).then(
+      (result: any) => {
+        this.isLoading = false;
+        console.log('adding user finished ', result);
+      }
+    );
+  }
+}
