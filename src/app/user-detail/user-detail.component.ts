430b48094d82284fee08ad4501a01df39336b648
Martin Bock <martinbock@arcor.de>
2025-03-17 17:48:32 +0100

    "user.details.component styles created"

--- src/app/user-detail/user-detail.component.ts
+++ src/app/user-detail/user-detail.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './user-detail.component.html',
-  styleUrl: './user-detail.component.scss'
+  styleUrl: './user-detail.component.scss',
 })
 export class UserDetailComponent {
+  firestore: Firestore = inject(Firestore);
+  userId = '';
 
+  constructor(private route: ActivatedRoute) {}
+
+ngOnInit() {
+  this.route.paramMap.subscribe(paramMap => {
+    const userId = paramMap.get('id');
+    console.log('Got ID :', userId);
+  });
+}
 }
